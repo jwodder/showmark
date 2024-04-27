2e6cdb984cbb54d8226db4f559189947b6b03718
John T. Wodder II <git@varonathe.org>
2024-04-27 08:54:41 -0400

    Packaging

--- src/showmark/__init__.py
+++ src/showmark/__init__.py
@@ -1,3 +1,15 @@
+"""
+Serve rendered markup documents
+
+Visit <https://github.com/jwodder/showmark> for more information.
+"""
+
+__version__ = "0.1.0.dev1"
+__author__ = "John Thorvald Wodder II"
+__author_email__ = "showmark@varonathe.org"
+__license__ = "MIT"
+__url__ = "https://github.com/jwodder/showmark"
+
 from collections import deque
 import io
 from pathlib import Path
